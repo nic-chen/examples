e651724c4537d15a45cf328f1a93b90bda5cc8be
JunXu Chen <chenjunxu6@gmail.com>
2023-06-09 15:08:34 +0800

    feat: add order API

--- shop/main.go
+++ shop/main.go
@@ -12,6 +12,11 @@ type Product struct {
 	Name string `json:"name"`
 }
 
+type Order struct {
+	ID int `json:"id"`
+	Product
+}
+
 var products = map[int]Product{
 	1: {
 		ID:   1,
@@ -20,6 +25,21 @@ var products = map[int]Product{
 	2: {
 		ID:   2,
 		Name: "Pear",
+	},
+	3: {
+		ID:   3,
+		Name: "Banana",
+	},
+}
+
+var orders = map[int]Order{
+	1: {
+		ID:      1,
+		Product: products[1],
+	},
+	2: {
+		ID:      2,
+		Product: products[3],
 	},
 }
 
@@ -48,7 +68,7 @@ func setupRouter() *gin.Engine {
 		id, err := strconv.Atoi(productID)
 		if err != nil {
 			c.JSON(400, gin.H{
-				"message": "invalid productId",
+				"message": "invalid id",
 			})
 			return
 		}
@@ -58,6 +78,32 @@ func setupRouter() *gin.Engine {
 		c.JSON(200, gin.H{
 			"code":    0,
 			"data":    products[id],
+			"version": version,
+		})
+	})
+
+	r.GET("/orders/:id", func(c *gin.Context) {
+		orderID := c.Param("id")
+		if orderID == "" {
+			c.JSON(400, gin.H{
+				"message": "id is required",
+			})
+			return
+		}
+
+		id, err := strconv.Atoi(orderID)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"message": "invalid id",
+			})
+			return
+		}
+
+		version := os.Getenv("VERSION")
+
+		c.JSON(200, gin.H{
+			"code":    0,
+			"data":    orders[id],
 			"version": version,
 		})
 	})
